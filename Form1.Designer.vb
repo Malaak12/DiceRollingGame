3dc26e917a22dbb70557fed2a8e75f8ef716d6ca
MalaakJaludi <121072340+Malaak12@users.noreply.github.com>
2024-05-13 11:12:08 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Button1 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(100, 150)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(200, 165)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Location = New Point(439, 150)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(200, 165)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(292, 402)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(150, 46)
+        Button1.TabIndex = 2
+        Button1.Text = "Roll"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(736, 543)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Dice Game"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim CountInteger As Integer
+    Dim PrefixString As String = "\images\die"
+    Dim SuffixString As String = ".png"
+    Dim ObjRandomInteger As Random = New Random
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If CountInteger < 15 Then
+            Roll(Dice1PictureBox)
+        End If
+        If CountInteger < 30 Then
+            Roll(Dice2PictureBox)
+        End If
+        CountInteger += 1
+    End Sub
+
+    Private Sub Roll(ByVal die As PictureBox)
+        Dim FaceInteger As Integer = ObjRandomInteger.Next(1, 7)
+        die.Image = Image.FromFile(Directory.GetCurrentDirectory & PrefixString & FaceInteger & SuffixString)
+    End Sub
+
+    Private Sub RollButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RollButton.Click
+        CountInteger = 0
+        Timer1.Enabled = True
+    End Sub
+End Class
